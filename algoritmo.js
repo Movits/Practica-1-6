4518b85305df6682e24753f98737bd7b5b5db66b
Roberto Alagemovits <robertomovits@gmail.com>
2023-04-08 01:54:05 -0300

    Practica 1-6(segunda parte)

--- algoritmo.js
+++ algoritmo.js
@@ -1,2 +1,15 @@
-let nombreIngresado = prompt("Escriba su nombre");
-document.write("Hola " + nombreIngresado + "!");
+let caracter = prompt("Ingrese un caracter");
+let string = prompt("Ingrese una string");
+let integer = prompt("Ingrese un integer");
+let float = prompt("Ingrese un float")
+let boolean = prompt("Ingrese un boolean")
+
+document.write("<h3> Caracter: " + caracter + "</h3>");
+document.write("<h3> String: " + string + "</h3>")
+document.write("<h3> Integer: " + integer + "</h3>")
+document.write("<h3> Float: " + float + "</h3>")
+document.write("<h3> Boolean: " + boolean + "</h3>")
+
+/* console.log (nombre);
+document.querySelector("body").innerHTML = "<h1>" +nombre + "</h1>";
+document.write("<h1>" +"Hola " + nombre + "</h1>"); */
